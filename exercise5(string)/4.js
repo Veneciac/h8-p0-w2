e6467c675329f40d40e451e4e36afe7f1921d672
Zhang <zhang@ZhangMBP15.local>
2018-09-25 14:08:48 +0700

    tugas 5

---
+++ exercise5(string)/4.js
@@ -0,0 +1,18 @@
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4,14);
+var thirdWord4 = word4.substring(15,17);
+var fourthWord4 = word4.substring(18,20);
+var fifthWord4 = word4.substring(21);
+
+var firstWordLength = exampleFirstWord4.length;
+var secwl = secondWord4.length;
+var thirdwl = thirdWord4.length;
+var fourthwl = fourthWord4.length;
+var fifthwl = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 +', with length: '+ secwl);
+console.log('Third Word: ' + thirdWord4+ ', with length: '+ thirdwl);
+console.log('Fourth Word: ' + fourthWord4+', with length: '+ thirdwl);
+console.log('Fifth Word: ' + fifthWord4+ ', with length: '+fifthwl);
