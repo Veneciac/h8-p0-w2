9d5b2156eb49e41ac4e8d1f3aedc09c04b0fb331
Zhang <zhang@Edithaaanugroho.localdomain>
2018-09-26 16:15:15 +0700

    7

---
+++ exercise7.js
@@ -0,0 +1,28 @@
+//1
+var star='*'
+var a= 0
+var row1 = 5
+
+while( a<= row1){
+  console.log(star);
+  a++;
+}
+
+
+//2
+var str =''
+for(var baris=0; baris<=5; baris++){
+  str += '*'
+}
+for (var colum=0; colum<=5; colum++){
+  console.log(str)
+}
+
+
+
+3
+var str1=''
+for(var baris2=1;baris2<=5; baris2++ ){
+  str1 +='*';
+  console.log(str1)
+}
